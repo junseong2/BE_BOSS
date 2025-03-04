c7bd5877bad55f1cd3e0ca4304878f9ace2cdacc
youngwan2 <qodna25@gmail.com>
2025-03-04 13:53:27 +0900

    chore: 판매자 도메인 폴더 구조 설정

---
+++ src/main/java/com/onshop/shop/seller/productManagement/ProductManagementServiceImpl.java
@@ -0,0 +1,10 @@
+package com.onshop.shop.seller.productManagement;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductManagementServiceImpl implements ProductManagementService {
+	
+	
+
+}
