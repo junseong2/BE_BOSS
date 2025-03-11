5c09edc5592ba91944dba47357dc105261753a56
junseong2 <ljs228811@naver.com>
2025-03-11 16:40:36 +0900

    feat : 다중 이미지 조회기능 추가

--- src/main/java/com/onshop/shop/products/ProductsDTO.java
+++ src/main/java/com/onshop/shop/products/ProductsDTO.java
@@ -1,41 +1,50 @@
-package com.onshop.shop.products;
-
-import lombok.*;
-
-import java.time.LocalDateTime;
-import com.onshop.shop.category.Category;
-
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-public class ProductsDTO {
+	package com.onshop.shop.products;
 	
+	import lombok.*;
+	
+	import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
-
-    private Long productId;
-    private Long categoryId;
-    private Long sellerId;
-    private String name;
-    private String description;
-    private Integer price;
-    private LocalDateTime expiryDate;
-    private LocalDateTime createdRegister;
-
- // ProductsDTO.java
-    public static ProductsDTO fromEntity(Product product) {
-        return ProductsDTO.builder()
-                .productId(product.getProductId())
-                .categoryId(product.getCategory() != null ? product.getCategory().getId() : null) // 수정된 부분
-                .sellerId(product.getSellerId())
-                .name(product.getName())
-                .description(product.getDescription())
-                .price(product.getPrice())
-                .expiryDate(product.getExpiryDate())
-                .createdRegister(product.getCreatedRegister())
-                .build();
-    }
-
-
-}
+import com.onshop.shop.category.Category;
+	
+	@Getter
+	@Setter
+	@NoArgsConstructor
+	@AllArgsConstructor
+	@Builder
+	public class ProductsDTO {
+		
+	
+	
+	    private Long productId;
+	    private Long categoryId;
+	    private Long sellerId;
+	    private String name;
+	    private String description;
+	    private Integer price;
+	    private List<String> gImage;
+	    private LocalDateTime expiryDate;
+	    private LocalDateTime createdRegister;
+	
+	 // ProductsDTO.java
+	    public static ProductsDTO fromEntity(Product product) {
+	    	List<String> imageUrls = product.getImageList().stream()
+	                .map(imageName -> "http://localhost:5000/uploads/" + imageName) // URL 변환
+	                .collect(Collectors.toList());
+	
+	        return ProductsDTO.builder()
+	                .productId(product.getProductId())
+	                .categoryId(product.getCategory() != null ? product.getCategory().getId() : null) // 수정된 부분
+	                .sellerId(product.getSellerId())
+	                .name(product.getName())
+	                .description(product.getDescription())
+	                .price(product.getPrice())
+	                .gImage(imageUrls)
+	                .expiryDate(product.getExpiryDate())
+	                .createdRegister(product.getCreatedRegister())
+	                .build();
+	    }
+	
+	
+	}
