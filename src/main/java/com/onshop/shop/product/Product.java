7ea5dc9c24b2d87c82353c0cf069ca39d5df13ab
= <spdlvka147@gmail.com>
2025-04-11 14:42:16 +0900

    feat 크롤링 백엔드 구현

--- src/main/java/com/onshop/shop/product/Product.java
+++ src/main/java/com/onshop/shop/product/Product.java
@@ -21,6 +21,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -57,15 +58,17 @@ public class Product {
     @Column(name="name", nullable = false)
     private String name;
     
-    @Column(name="description")
+    @Lob
+    @Column(name = "description", columnDefinition = "TEXT")
     private String description;
 
     @Column(name="price")
     private Integer price;
     
-    @Column(name="gImage")
+    @Lob
+    @Column(name = "gImage", columnDefinition = "TEXT")
     private String gImage;
-
+    
     @Column(name="expiry_date")
     private LocalDateTime expiryDate;
 
